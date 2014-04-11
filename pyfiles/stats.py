ae27241a64be357394dd6e353cb3c9fa3c33a8e1
Nick J. Amin <amin.nj@elara.physics.tamu.edu>
2014-04-11 16:48:23 -0500

    added histogram option to stats.py

--- pyfiles/stats.py
+++ pyfiles/stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import math, sys
+import math, sys, os
 
 def statistics(ls):
     length = len(ls)
@@ -31,4 +31,9 @@ if __name__ == "__main__":
         min: {4}
         max: {5}
         """.format(*statistics(nums))
+
+    if(len(sys.argv) > 2):
+        if(sys.argv[-2].strip() == "-h"):
+            binwidth=sys.argv[-1]
+            os.system("gnuplot -e 'set term dumb; binwidth=%s; bin(x,width)=width*floor(x/width); plot \"L1B.dat\" using (bin($1,binwidth)):(1.0) smooth freq with boxes'" % (binwidth))
 
