490bcc014dc2f2f63d9396cd97f96bbd644ee576
Nick Amin <namin@squark.physics.ucsb.edu>
2015-08-15 18:05:48 -0700

    bug fix with monitor and add fine/grainy scores

--- miscfiles/cmsmonitor/monitor.py
+++ miscfiles/cmsmonitor/monitor.py
@@ -34,17 +34,24 @@ rgbimg = imgCMS.convert("RGB")
 for i in range(14):
     coords = (498,374+i*dy)
     rgb = rgbimg.getpixel(coords)
-    if(rgb[1] > 240): systemsin.append(systemsall[i])
+    if(250 > rgb[1] > 240): systemsin.append(systemsall[i])
 
 for i in range(14):
     coords = (556,374+i*dy)
     rgb = rgbimg.getpixel(coords)
-    if(rgb[1] > 240): systemson.append(systemsall[i])
+    if(250 > rgb[1] > 240): systemson.append(systemsall[i])
 
+numSystemsgood = 0
 for system in ["CSC","DT","ECAL","ES","HCAL","PIXEL","RPC","TRACKER","TRG","DAQ","DQM","SCAL","HF"]:
-    if system not in systemsin: systemsgood = False
+    if system not in systemsin:
+        systemsgood = False
+    else:
+        numSystemsgood += 1
 for system in ["CSC","DT","ECAL","ES","HCAL","PIXEL","RPC","TRACKER"]:
-    if system not in systemson: systemsgood = False
+    if system not in systemson:
+        systemsgood = False
+    else:
+        numSystemsgood += 1
 
 print systemsin, systemson, systemsgood
 
@@ -135,9 +142,24 @@ info["good"] = {}
 info["good"]["beamsgood"] = "stable" in beam
 info["good"]["systemsgood"] = systemsgood
 info["good"]["bfieldgood"] = bfield > 3.7
-info["good"]["energygood"] = energy > 6400
+info["good"]["energygood"] = energy > 6390
 info["good"]["realtime"] = int((datetime.datetime.now() + datetime.timedelta(hours=9)).strftime("%s"))
 info["good"]["timestampparsed"] = timestampparsed
+
+info["score"] = {}
+
+
+fractionalSystemsGood = 1.0*numSystemsgood/21
+fractionalEnergy = 1.0*energy/6400
+fractionalBfield = 1.0*bfield/3.8
+
+# 3 points for full energy, 5 for full B, 5 for full systems, 2 for stable beams
+scorefine = 3.0*fractionalEnergy + 5.0*fractionalBfield + 5.0*fractionalSystemsGood + 2.0*("stable" in beam)
+scoregrainy = 5*systemsgood + 5*(bfield>3.7) + 5*("stable" in beam)
+info["scorefine"] = scorefine
+info["scoregrainy"] = scoregrainy
+
+
 
 print json.dumps(info, indent=4)
 out = open("monitor.json","w")
@@ -152,6 +174,6 @@ beamsgood = good["beamsgood"]
 bfieldgood = good["bfieldgood"]
 systemsgood = good["systemsgood"]
 out = open("data.txt","a")
-out.write("%i %i %i %i %i %i\n" % (realtime,timestampparsed,energygood,beamsgood,bfieldgood,systemsgood))
+out.write("%i %i %i %i %i %i %i %.1f\n" % (realtime,timestampparsed,energygood,beamsgood,bfieldgood,systemsgood,scoregrainy,scorefine) )
 out.close()
 
