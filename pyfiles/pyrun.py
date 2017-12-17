bcd720516a2784929eac23d87bba5bbce7c91224
amarang <namin@physics.ucsb.edu>
2017-12-17 13:21:00 -0800

    better documentation, outputs, and customizable function

--- pyfiles/pyrun.py
+++ pyfiles/pyrun.py
@@ -6,13 +6,39 @@ import sys
 import commands
 import argparse
 
-from tqdm import format_meter, StatusPrinter
+try:
+    from tqdm import format_meter, StatusPrinter
+except:
+    from pyfiles.tqdm import format_meter, StatusPrinter
+
 from multiprocessing import Pool
 
-
 """
-# Usage:
+# Execute bash commands like xargs but with a prettier output
+
+# Use 6 processes to sleep random amounts (piping in commands)
 $ for i in `seq 1 50`; do echo sleep $(echo "$RANDOM/15000" | bc -l); done | pyrun.py -n 6
+
+# Put commands in a file and use that as a command list
+$ for i in `seq 1 50`; do echo sleep $(echo "$RANDOM/15000" | bc -l); done > cmds.sh
+$ pyrun.py -f cmds.sh
+
+# Use python API (basically a wrapper around multiprocessing libraries)
+>>> from pyrun import Runner
+>>> runner = Runner(nproc=5)
+>>> runner.add_args(["sleep 1", "sleep 2", "sleep 3", "sleep 4"])
+>>> runner.run()
+
+# But what if I want to execute python functions and not shell commands?!
+# Note that the function must take an index and then the args, for bookkeeping
+>>> from pyrun import Runner
+>>> def my_func((idx,args)):
+        return idx, sum(args)
+>>> runner = Runner(nproc=5, func=my_func)
+>>> runner.add_args([(1,2),(3,4),(4,5),(6,7)])
+>>> runner.run()
+>>> print runner.get_outputs()
+[(0, 3), (1, 7), (2, 9), (3, 13)]
 """
 
 def f_test((idx,x)):
@@ -25,7 +51,7 @@ def f_bash((idx,x)):
     return idx,status
 
 class Runner(object):
-    def __init__(self,nproc):
+    def __init__(self,nproc=5,func=f_bash):
         self.pool = Pool(processes=nproc)
         self.args = []
         self.indices_with_args = []
@@ -34,7 +60,8 @@ class Runner(object):
         self.ndone = 0
         self.elapsed = 0
         self.t0 = None
-        self.func = f_bash
+        self.func = func
+        self.outputs = []
 
     def add_args(self,args):
         next_idx = len(self.indices_with_args)
@@ -53,6 +80,11 @@ class Runner(object):
             yield self.indices_with_args[i]
             i += 1
 
+    def get_outputs(self):
+        # Return list of pairs (first element is the job index,
+        # second element is the return value of the function)
+        return self.outputs
+
     def run(self):
         if not self.t0: 
             self.t0 = time.time()
@@ -60,6 +92,7 @@ class Runner(object):
         sp = StatusPrinter(sys.stderr)
         for idx,ret in self.pool.imap_unordered(self.func,self.get_args()):
             self.indices_status[idx] = True
+            self.outputs.append((idx,ret))
             # Try to add another arg, but won't work (see reason in get_args)
             # if self.ndone == 6:
             #     self.add_args(["sleep 2"])
@@ -79,13 +112,12 @@ if __name__ == '__main__':
     func_args = []
     if cli_args.file:
         fname = cli_args.file
-        func = f_bash
-        for line in fname:
+        for line in open(fname,"r"):
             if line.strip():
                 func_args.append(line.strip())
+        print func_args
     else:
         for item in sys.stdin:
-            func = f_bash
             func_args.append(item.strip())
 
     runner = Runner(int(cli_args.nproc))
