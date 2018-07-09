51fe72ac186e1d7549b6adf293295b898e4e5c27
Nick Amin <amin.nj@gmail.com>
2018-07-08 20:19:12 -0700

    handle negative numbers too

--- pyfiles/natsort.py
+++ pyfiles/natsort.py
@@ -4,7 +4,13 @@ import sys
 import re
 
 def natsort(s):
-    return [int(t) if t.isdigit() else t.lower() for t in re.split('(\d+)', s)]
+
+    def soft_int(t):
+        try:
+            return int(t)
+        except:
+            return t.lower()
+    return map(soft_int,re.split('([\-\+]\d+)', s))
 
 if __name__ == "__main__":
     items = []
